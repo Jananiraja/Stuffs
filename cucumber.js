5e80c807170f7451635f53b904d856127b492021
JAYAARV <jayanthijannai.arv@daimler.com>
2022-02-28 11:25:40 +0530

    modified package.json

---
+++ cucumber.js
@@ -0,0 +1,12 @@
+let common = [
+    "features/**/*.feature",
+    "--require-module ts-node/register",
+    "--require ./src/steps/*.ts",
+    "--require ./src/hooks/hooks.ts",
+    "--format json:reports/cucumber-report.json",
+  ].join(' ');
+  
+  module.exports = {
+    default: common
+    // More profiles can be added if desired
+  };
